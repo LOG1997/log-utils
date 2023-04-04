e3592f32538513e0b40649bf2ef8ca344044b4d1
zhangwenlei <2694233102@qq.com>
2023-04-04 14:04:31 +0800

    feat: deploy

---
+++ packages/.vitepress/theme/components/Contributors.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+// import { computed } from 'vue';
+
+const props = defineProps<{ id: string }>();
+console.log('😊props:', props);
+const contributors = [
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar:
+      'https://p1-jj.byteimg.com/tos-cn-i-t2oaga2asx/gold-user-assets/2018/11/15/167161416e6af0ea~tplv-t2oaga2asx-no-mark:180:180:180:180.awebp',
+    name: 'log1997',
+  },
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar: 'http://10.12.3.86/assets/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png',
+    name: 'log1997',
+  },
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar: 'http://10.12.3.86/assets/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png',
+    name: 'log1997',
+  },
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar: 'http://10.12.3.86/assets/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png',
+    name: 'log1997',
+  },
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar: 'http://10.12.3.86/assets/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png',
+    name: 'log1997',
+  },
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar: 'http://10.12.3.86/assets/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png',
+    name: 'log1997',
+  },
+  {
+    hash: '123',
+    login: 'log1997',
+    avatar: 'http://10.12.3.86/assets/no_avatar-849f9c04a3a0d0cea2424ae97b27447dc64a7dbfae83c036c45b403392f0e8ba.png',
+    name: 'log1997',
+  },
+];
+</script>
+
+<template>
+  <div class="contributors-container">
+    <h2>贡献者</h2>
+    <div class="contributors-list">
+      <div v-for="c of contributors" :key="c.hash">
+        <a :href="`https://github.com/${c.login}`" class="link" no-icon>
+          <img :src="c.avatar" class="w-8 h-8 rounded-full" />
+          <span>{{ c.name }}</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.contributors-container {
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+}
+.contributors-container h2 {
+  font-size: 1.5rem;
+  font-weight: 500;
+  margin-bottom: 1rem;
+}
+.contributors-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+.link {
+  color: var(--text-color-light);
+  display: flex;
+  align-items: center;
+}
+.link img {
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+}
+.link span {
+  margin-left: 0.5rem;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // vite.config.ts
-import path from "path";
-import { defineConfig } from "vite";
-import typescript from "@rollup/plugin-typescript";
+import path from 'path';
+import { defineConfig } from 'vite';
+import typescript from '@rollup/plugin-typescript';
 
-import viteCompression from "vite-plugin-compression";
+import viteCompression from 'vite-plugin-compression';
 
-import removeConsole from "vite-plugin-remove-console";
+import removeConsole from 'vite-plugin-remove-console';
 function resolve(str: string) {
   return path.resolve(__dirname, str);
 }
@@ -15,27 +15,27 @@ export default defineConfig({
     removeConsole(),
     viteCompression(),
     typescript({
-      target: "es5",
-      rootDir: resolve("packages/"),
+      target: 'es5',
+      rootDir: resolve('packages/utils'),
       declaration: true,
-      declarationDir: resolve("lib"),
-      exclude: resolve("node_modules/**"),
+      declarationDir: resolve('lib'),
+      exclude: resolve('node_modules/**'),
       allowSyntheticDefaultImports: true,
     }),
   ],
   build: {
     // 打包输出的目录
-    outDir: "lib",
+    outDir: 'lib',
 
     // 防止 vite 将 rgba() 颜色转化为 #RGBA 十六进制
     // cssTarget: 'chrome61',
     lib: {
       // 组件库源码的入口文件
-      entry: resolve("packages/index.ts"),
+      entry: resolve('packages/utils/index.ts'),
       // 组件库名称
-      name: "getRandomGradientColor",
+      name: 'getRandomGradientColor',
       // 文件名称, 打包结果举例: my-packages.umd.cjs
-      fileName: "index",
+      fileName: 'index',
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
