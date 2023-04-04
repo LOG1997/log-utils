df243f9a868b366095fb1c92a6d4282a7b9eebd4
LOG1997 <LOG1997@users.noreply.github.com>
2023-04-04 06:41:13 +0000

    deploy: 7ff40c2822125268c796e3bfdd5e579b529cec99

--- lib/colorFunc/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './formatColor';
-export * from './randomGradientColor';
--- packages/.vitepress/theme/composables/authors.ts
+++
@@ -1,4 +0,0 @@
-import { computed } from 'vue';
-import { useData } from 'vitepress';
-
-import type { PageData } from 'vitepress';
